b505178a3b16f749f87855a4a66b01fd63ac64a2
Mark Go <mark.vince.go@gmail.com>
2020-04-25 00:36:07 -0700

    has both Mark and Kyle components.

---
+++ Components/Layout/Navigation.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import {BottomNavigation, Text} from 'react-native-paper';
+import StartBrowsePage from '../StartBrowsePage/StartBrowsePage';
+import RestaurantsPage from '../RestaurantInfoPage/RestaurantInfoPage';
+import OwnerMenuView from '../OwnerMenuView/OwnerMenuView'
+
+const RestaurantsInfoRoute = () => <StartBrowsePage />;
+
+const SearchRoute = () => <Text>Search</Text>;
+
+const OwnerRoute = () => <OwnerMenuView />;
+
+export default class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      routes: [
+        {key: 'browse', title: 'Browse', icon: 'queue-music'},
+        {key: 'search', title: 'Search', icon: 'album'},
+        {key: 'owner', title: 'Owner', icon: 'history'},
+      ],
+    };
+    this._renderScene = BottomNavigation.SceneMap({
+      browse: RestaurantsInfoRoute,
+      search: SearchRoute,
+      owner: OwnerRoute,
+    });
+    this._handleIndexChange = this._handleIndexChange.bind(this);
+  }
+
+  _handleIndexChange(index) {
+    this.setState({index});
+  }
+
+  render() {
+    return (
+      <BottomNavigation
+        navigationState={this.state}
+        onIndexChange={this._handleIndexChange}
+        renderScene={this._renderScene}
+        shifting
+        sceneAnimationEnabled
+      />
+    );
+  }
+}
--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 /**
  * @format
  */
-
-import { AppRegistry } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
+import 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#9c1f1f',
+    accent: 'dbc500',
+  },
+};
+
+export default function Main() {
+  return (
+    <NavigationContainer>
+      <PaperProvider theme={theme}>
+        <App theme={theme} />
+      </PaperProvider>
+    </NavigationContainer>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
