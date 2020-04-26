966d3689606215fa8eb7e7932a7ab28c7802225d
Kyle Nguyen <kylenguyen@kn-macbook-pro.lan>
2020-04-25 23:41:20 -0700

    added missing components back

--- Components/OwnerMenuView/AddItemMenu.js
+++ Components/OwnerMenuView/AddItemMenu.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text, withTheme } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { Text, Subheading, withTheme, Button } from 'react-native-paper';
+import ItemNameForm from './ItemNameForm';
+import AllergyForm from './AllergyForm';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+const styles = StyleSheet.create({
+  submitButton: {
+    width: wp('25%'),
+    height: hp('4.5%'),
+    justifyContent: 'center',
+    alignSelf: 'center',
+    margin: '5%'
+  },
+  titles: {
+    alignItems: 'center'
+  },
+  container: {
+    flex: 1,
+  }
+})
 
 class AddItemMenu extends React.Component {
-
   render() {
     return (
-      <View>
-        <Text> TESTING </Text>
+      <View styles={styles.container}>
+        <Subheading styles={styles.titles}>Item Name</Subheading>
+        <ItemNameForm id="Item Nameeeee"/>
+        <Subheading styles={styles.titles}>Category</Subheading>
+        <ItemNameForm />
+        <Subheading styles={styles.titles}>Description</Subheading>
+        <ItemNameForm />
+        <Subheading>Allergies</Subheading>
+        <AllergyForm />
+        <Button
+          style={styles.submitButton}
+          icon="plus"
+          mode="contained"
+          onPress={() => console.log('submitted')}
+        >
+          Submit
+        </Button>
       </View>
     )
   }
