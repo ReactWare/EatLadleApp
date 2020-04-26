856c931581302c280106ae7b63cb3f9a4bc34e44
Mark Go <mark.vince.go@gmail.com>
2020-04-25 20:02:41 -0700

    link browse to restaurant list to restaurant page

--- Components/RestaurantInfoPage/RestaurantInfoPage.js
+++ Components/RestaurantInfoPage/RestaurantInfoPage.js
@@ -8,18 +8,11 @@ const style = StyleSheet.create({
   wrapper: {
     flex: 1,
   },
-  topAppBar: theme => ({
-    height: 30,
-    backgroundColor: theme.colors.primary,
-  }),
 });
 
 const RestaurantPage = ({theme}) => {
   return (
     <View style={style.wrapper}>
-      <Appbar.Header>
-        <Appbar.Content title="Eat Ladle" />
-      </Appbar.Header>
       <PicGallery />
       <MenuScroll />
     </View>
