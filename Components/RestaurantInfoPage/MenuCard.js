bed3c1ae5b922561c2b2078400cd6f2d14085046
Mark Go <mark.vince.go@gmail.com>
2020-04-26 05:43:16 -0700

    Add checkout button to kits. Add fake data server for testing.

--- Components/RestaurantInfoPage/MenuCard.js
+++ Components/RestaurantInfoPage/MenuCard.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Card, List, Paragraph} from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { Card, List, Paragraph, Button } from 'react-native-paper';
 
 
 const style = StyleSheet.create({
   card: {
     marginBottom: 2,
+    paddingLeft: 0,
   },
+  content: {
+    paddingLeft: 30,
+  },
+  button: {
+    justifyContent: 'center',
+  }
 });
 
 const MenuScroll = ({ item, pressFn, index, data }) => (
   <List.Accordion
     title={item.name}
-    left={props => <List.Icon {...props} icon="folder" />}
+    left={props => <List.Icon {...props} icon="food-fork-drink" />}
     onPress={() => pressFn(index)}
   >
     <Card style={style.card}>
-      <Card.Content>
+      <Card.Content style={style.content}>
         {item.content.map((itemIndex) => (
           <Paragraph>{`x   ${data.menu.items[itemIndex].name}`}</Paragraph>
-        ))
-        }
+        ))}
       </Card.Content>
+      <Card.Actions style={style.button}>
+        <Button mode="contained">CHECKOUT</Button>
+      </Card.Actions>
     </Card>
   </List.Accordion>
 );
